e0697bf696c7be1495efd8f7fb78fa1dec179f35
wangbor <wangbor@yeah.net>
2018-08-10 10:16:38 +0800

    migrate from netty-server repository

---
+++ netty-spring-webmvc/src/main/java/com/github/berrywang1996/netty/spring/web/mvc/bind/annotation/GetMapping.java
@@ -0,0 +1,37 @@
+/*
+ * Copyright 2018 berrywang1996
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.github.berrywang1996.netty.spring.web.mvc.bind.annotation;
+
+import com.github.berrywang1996.netty.spring.web.mvc.bind.consts.HttpRequestMethod;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * @author berrywang1996
+ * @version V1.0.0
+ */
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+@RequestMapping(method = HttpRequestMethod.GET)
+public @interface GetMapping {
+
+    String[] value() default {};
+
+}
---
+++ netty-spring-websocket/src/main/java/com/github/berrywang1996/netty/spring/web/socket/bind/annotation/MessageMapping.java
@@ -0,0 +1,64 @@
+/*
+ * Copyright 2018 berrywang1996
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.github.berrywang1996.netty.spring.web.socket.bind.annotation;
+
+import com.github.berrywang1996.netty.spring.web.socket.bind.consts.MessageConsumerEndpoint;
+import com.github.berrywang1996.netty.spring.web.socket.bind.consts.MessageProducerEndpoint;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * <pre>
+ * /-----------------------------------------------------------------------------------\
+ * | PRODUCER | CONSUMER | PIPELINE                                                    |
+ * |----------|----------|-------------------------------------------------------------|
+ * | ONE      | ONE      | one user send message -> server -> one user receive message |
+ * |----------|----------|-------------------------------------------------------------|
+ * | ONE      | MANY     | one user send message -> server -> all user receive message |
+ * |----------|----------|-------------------------------------------------------------|
+ * | SERVICE  | ONE      | server send message   -> single user receive message        |
+ * |----------|----------|-------------------------------------------------------------|
+ * | SERVICE  | MANY     | server send message   -> all user receive message           |
+ * \-----------------------------------------------------------------------------------/
+ * </pre>
+ *
+ * @author berrywang1996
+ * @version V1.0.0
+ */
+@Target({ElementType.METHOD})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface MessageMapping {
+
+    /**
+     * url
+     */
+    String value() default "";
+
+    /**
+     * message producer
+     */
+    MessageProducerEndpoint producer() default MessageProducerEndpoint.ONE;
+
+    /**
+     * message consumer
+     */
+    MessageConsumerEndpoint consumer() default MessageConsumerEndpoint.ONE;
+
+}
