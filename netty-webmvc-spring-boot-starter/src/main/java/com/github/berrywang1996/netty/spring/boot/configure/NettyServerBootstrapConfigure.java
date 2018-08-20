62b93c3ee2684eda9a9dc6e6d1031ba38493f1e4
wangbor <wangbor@yeah.net>
2018-08-20 15:21:22 +0800

    split module named netty-web-spring-boot-starter

---
+++ netty-webmvc-spring-boot-starter/src/main/java/com/github/berrywang1996/netty/spring/boot/configure/NettyServerBootstrapConfigure.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright 2018 berrywang1996
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.github.berrywang1996.netty.spring.boot.configure;
+
+import com.github.berrywang1996.netty.spring.web.startup.NettyServerBootstrap;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author berrywang1996
+ * @since V1.0.0
+ */
+@Slf4j
+@Configuration
+public class NettyServerBootstrapConfigure {
+
+    @Autowired
+    private NettyServerStartupPropertiesWrapper startupProperties;
+
+    @Autowired
+    private ApplicationContext applicationContext;
+
+    @Bean
+    public NettyServerBootstrap nettyServer() {
+        final NettyServerBootstrap nettyServerBootstrap = new NettyServerBootstrap(applicationContext);
+        try {
+            nettyServerBootstrap.start(startupProperties);
+        } catch (Exception e) {
+            log.error("Netty startup failed!", e);
+            System.exit(1);
+        }
+        return nettyServerBootstrap;
+    }
+
+}
